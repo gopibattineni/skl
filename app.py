4ffd5f5ade978a686cff277f87938643b73634a5
Gopi Battineni <117643410+gopibattineni@users.noreply.github.com>
2025-02-27 20:25:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -87,4 +87,5 @@ def upload():
         return result
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use Render's port or default to 5000
+    app.run(host="0.0.0.0", port=port, debug=True)
